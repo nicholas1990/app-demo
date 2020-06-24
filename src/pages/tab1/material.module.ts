cc975223edc835acde5c5d1500b23d7c04efb45c
Nicholas <nicholas.angelucci@gmail.com>
2020-06-24 11:57:39 +0200

    [clean] - tab 1
    Signed-off-by: Nicholas <nicholas.angelucci@gmail.com>

--- src/pages/tab1/material.module.ts
+++ src/pages/tab1/material.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 
 // Angular Material Components
-import { MatListModule, MatListItem } from '@angular/material/list';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [],
